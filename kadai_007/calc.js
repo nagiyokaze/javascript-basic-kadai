95027ded7986271c07472f29cc379fb8b19c1940
nagiyokaze <158305309+nagiyokaze@users.noreply.github.com>
2024-04-15 20:16:11 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+let num = 30;
+
+// 変数numが3と5の倍数の場合
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("3と5の倍数です");
+}
+// 変数numが3の倍数の場合
+else if (num % 3 === 0) {
+    console.log("3の倍数です");
+}
+// 変数numが5の倍数の場合
+else if (num % 5 === 0) {
+    console.log("5の倍数です");
+}
+// それ以外の場合
+else {
+    console.log(num);
+}
